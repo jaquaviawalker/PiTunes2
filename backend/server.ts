97ff084fb6f592c07ca5338a4881f4e446ca4b54
Jaquavia Walker <jaquavia.walker@copeland.com>
2025-10-07 16:30:11 -0400

    import cors to backend

--- backend/server.ts
+++ backend/server.ts
@@ -2,9 +2,13 @@ import express, { Request, Response } from 'express';
 import UserRoutes from './routes/UserRoutes';
 import MFRC522Routes from './routes/MFRC522Routes';
 import PlaybackRoutes from './routes/PlaybackRoutes';
+import cors from 'cors';
 const app = express();
+
 const port = 3000;
+app.use(cors());
 app.use(express.json());
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
